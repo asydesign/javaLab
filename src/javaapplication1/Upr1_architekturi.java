ae1a6f107d8c85eda2687a927c80d7604589cad1
Asia <Asia@Asia-PC>
2013-11-25 16:23:43 +0200

    Merge origin/master
    Conflicts:
    	build.xml
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	src/javaapplication1/FirstStart.java

--- src/javaapplication1/Upr1_architekturi.java
+++ src/javaapplication1/Upr1_architekturi.java
@@ -1,78 +1,75 @@
 package javaapplication1;
+
 import java.util.Scanner;
-import javax.swing.*; //awt
+import javax.swing.*; //or awt - graphycs interface
 //http://pastebin.com/u/wingman007
 
 public class Upr1_architekturi {
-   
-    public static void Choose(int zadacha){
-        switch(zadacha){
+
+    public static void Choose(int zadacha) {
+        switch (zadacha) {
             case 1:
                 myMetod1();
                 break;
-            case 2:    
-               myMetod2();
+            case 2:
+                myMetod2();
                 break;
-            
-             default: 
-                 System.out.print("Error!");
+
+            default:
+                System.out.print("Error!");
                 break;
-        } 
+        }
     }
-    
-    public static void myMetod1(){
-   
+
+    public static void myMetod1() {
+// преобразуване на числа в десетична бр. с-ма
         Scanner KeyS = new Scanner(System.in);
         String ns;
         System.out.println("Въведи число: ");
-        ns=KeyS.nextLine();
-    
+        ns = KeyS.nextLine();
+
         int s;
-        System.out.println("Въведи бройна система: ");
-        s=KeyS.nextInt();
-        
-        int a=0;
-        for(int i=0; i < ns.length(); i++){
-            a = a*s + Integer.parseInt(String.valueOf(ns.charAt(i)));
+        System.out.println("В каква бройна система е това число: ");
+        s = KeyS.nextInt();
+
+        int a = 0;
+        for (int i = 0; i < ns.length(); i++) {
+            a = a * s + Integer.parseInt(String.valueOf(ns.charAt(i)));
         }
-        System.out.println("Числото в десетична с-ма е: "+ a);
+        System.out.println(s+"-ичното число в десетична с-ма е: " + a);
     }
-    
-    public static void myMetod2(){
-   
+
+    public static void myMetod2() {
+// преобразуване на десетични числа в друга бр. с-ма
         Scanner KeyS = new Scanner(System.in);
         int ns;
-        System.out.println("Въведи число в десетична с-ма: ");
+        System.out.println("Въведи цяло десетично число: ");
         ns = KeyS.nextInt();
-    
-        int s; 
-        System.out.println("Въведи бройна система: ");
+
+        int s;
+        System.out.println("В каква бройна система да превърна числото: ");
         s = KeyS.nextInt();
-       
-      
-       int[] a = new int[100];
-       int k;
-       int i=0;
-       String myBinary = " ";
-       int first_ns = ns;
-       
-       while ((ns-s)>=0){ 
-          ns = ns/s;
-           k = ns%s;
-                    System.out.println();    
 
-           a[i] = k;
-           i=i+1;
-       }
-       a[i]=ns;
+        String a = "";
+        int k = 0;
+        String myBinary = "";
+        int first_ns = ns;
 
-//      for(int j = i; j>=0; j--){ //a.length = 100
-//        
-//        myBinary = myBinary + a[j];
-//      }  
-//      
-//       System.out.println("Числото "+first_ns+" в "+ s +"-ична с-ма е: " + myBinary);
-       
-      
+        while (ns > (s-1)) {
+            k = ns % s;
+            ns = ns / s;
+            a = a + Integer.toString(k);
+
+        }
+        if ((ns <= (s-1))&&(ns>0)) {
+            k = ns;
+        }
+        
+        
+        a = a + Integer.toString(k);
+        for (int i = a.length() - 1; i >= 0; i--) {
+            myBinary = myBinary + a.charAt(i);
+        }
+        System.out.println("Числото " + first_ns + " в " + s + "-ична с-ма е: " + myBinary);
     }
 }
